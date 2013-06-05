4ee61c6dce7b28f73c8c3a0b962b14a26943037e
alpha <wolfgang.schuessel@gmail.com>
2013-06-05 15:38:46 +0200

    wp

---
+++ lib/Yafirst/SysArch/index_adapter.rb
@@ -0,0 +1,26 @@
+module Yafirst::SysArch
+
+  class IndexAdapter
+    attr_reader :config
+
+    def initialize(params)
+
+      if params[:config]
+        @config = params[:config]
+      end
+    end
+
+    def add_filter(params)
+
+    end
+
+    def search
+
+    end
+
+    def result
+['woofer!:',' alles',:bestens]
+    end
+  end
+
+end
